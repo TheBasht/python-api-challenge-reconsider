bb07c84fd16f52b76bea435f006feca95610c2a2
TheBasht <90795605+TheBasht@users.noreply.github.com>
2024-08-02 00:00:25 -0600

    Add files via upload
    Please note what the previous grade was and the reason for it. I added an input that lets you choose the amount of cities, its not because the code is not good... this is because api at the time of testing was taking HOURS to process many cities. Heres updated repo with 500+ cities after letting it run for hours.

---
+++ python-api-challenge/README.md
@@ -0,0 +1,1 @@
+The weather api had an input added to it so we can limit the amount of cities because it takes forever to test.
---
+++ python-api-challenge/WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "5c64d8248490375f03472512af6a7084"
+
+# Geoapify API Key
+geoapify_key = "cf73032aa1364b7491d9bd240d5a73ea"
